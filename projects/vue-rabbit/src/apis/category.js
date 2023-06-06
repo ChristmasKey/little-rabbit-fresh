ddc65af7c5287b859980643d74d1746ef48808f5
丁佳男 <jiananst@qq.com>
2023-06-07 00:58:55 +0800

    二级分类

--- projects/vue-rabbit/src/apis/category.js
+++ projects/vue-rabbit/src/apis/category.js
@@ -1,5 +1,6 @@
 import httpInstance from "@/utils/http";
 
+// 获取一级分类列表数据
 export function getCategoryAPI(id) {
     return httpInstance({
         url: '/category',
@@ -7,5 +8,25 @@ export function getCategoryAPI(id) {
         params: {
             id
         }
+    })
+}
+
+// 获取二级分类列表数据
+export function getCategoryFilterAPI(id) {
+    return httpInstance({
+        url: '/category/sub/filter',
+        method: 'get',
+        params: {
+            id
+        }
+    })
+}
+
+// 获取商品列表数据
+export function getSubCategoryAPI(data) {
+    return httpInstance({
+        url: '/category/goods/temporary',
+        method: 'post',
+        data
     })
 }
--- projects/vue-rabbit/src/router/index.js
+++ projects/vue-rabbit/src/router/index.js
@@ -1,35 +1,46 @@
 // createRouter：创建router实例对象
 // createWebHistory：创建history模式的路由
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Layout from "@/views/Layout/index.vue"
 import Login from "@/views/Login/index.vue"
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
 
-  // path和component对应关系的位置
-  routes: [
-    {
-      path: '/',
-      component: Layout,
-      children: [
+    // path和component对应关系的位置
+    routes: [
         {
-          path: '', //由于Home是默认渲染的页面，所以我们可以将其路由置空，以此达到默认渲染此页面的效果
-          component: Home
+            path: '/',
+            component: Layout,
+            children: [
+                {
+                    path: '', //由于Home是默认渲染的页面，所以我们可以将其路由置空，以此达到默认渲染此页面的效果
+                    component: Home
+                },
+                {
+                    path: '/category/:id',
+                    component: Category
+                },
+                {
+                    path: '/category/sub/:id',
+                    component: SubCategory
+                }
+            ]
         },
         {
-          path: '/category/:id',
-          component: Category
+            path: '/login',
+            component: Login
         }
-      ]
-    },
-    {
-      path: '/login',
-      component: Login
+    ],
+    // 路由滚动行为定制
+    scrollBehavior() {
+        return {
+            top: 0
+        }
     }
-  ]
 })
 
 export default router
